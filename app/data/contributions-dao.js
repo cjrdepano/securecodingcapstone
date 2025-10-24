6f59eef0594d5a9d2b1cf97e3e18484af01a49a5
cjrdepano <cjrdepano@gmail.com>
2025-10-24 15:18:58 +0800

    fix: remove eval() code injection; harden contributions DAO

--- app/data/contributions-dao.js
+++ app/data/contributions-dao.js
@@ -2,85 +2,83 @@ const UserDAO = require("./user-dao").UserDAO;
 
 /* The ContributionsDAO must be constructed with a connected database object */
 function ContributionsDAO(db) {
-    "use strict";
+  "use strict";
 
-    /* If this constructor is called without the "new" operator, "this" points
-     * to the global object. Log a warning and call it correctly. */
-    if (false === (this instanceof ContributionsDAO)) {
-        console.log("Warning: ContributionsDAO constructor called without 'new' operator");
-        return new ContributionsDAO(db);
-    }
+  /* If this constructor is called without the "new" operator, "this" points
+   * to the global object. Log a warning and call it correctly. */
+  if (false === (this instanceof ContributionsDAO)) {
+    // eslint-disable-next-line no-console
+    console.log("Warning: ContributionsDAO constructor called without 'new' operator");
+    return new ContributionsDAO(db);
+  }
 
-    const contributionsDB = db.collection("contributions");
-    const userDAO = new UserDAO(db);
+  const contributionsDB = db.collection("contributions");
+  const userDAO = new UserDAO(db);
 
-    this.update = (userId, preTax, afterTax, roth, callback) => {
-        const parsedUserId = parseInt(userId);
+  /**
+   * Upsert contribution percentages for a user.
+   */
+  this.update = (userId, preTax, afterTax, roth, callback) => {
+    const parsedUserId = parseInt(userId, 10);
 
-        // Create contributions document
-        const contributions = {
-            userId: parsedUserId,
-            preTax: preTax,
-            afterTax: afterTax,
-            roth: roth
+    // Always store a consistent numeric userId and only the fields we expect
+    const doc = {
+      userId: parsedUserId,
+      preTax,
+      afterTax,
+      roth,
+    };
+
+    contributionsDB
+      .updateOne(
+        { userId: parsedUserId }, // match on numeric id to avoid type mismatches
+        { $set: doc },
+        { upsert: true }
+      )
+      .then(() => {
+        // add user details for rendering
+        userDAO.getUserById(parsedUserId, (err, user) => {
+          if (err) return callback(err, null);
+
+          const result = {
+            ...doc,
+            userName: user.userName,
+            firstName: user.firstName,
+            lastName: user.lastName,
+          };
+          return callback(null, result);
+        });
+      })
+      .catch((err) => callback(err, null));
+  };
+
+  /**
+   * Get contribution percentages for a user. If none exist, return defaults.
+   */
+  this.getByUserId = (userId, callback) => {
+    const parsedUserId = parseInt(userId, 10);
+
+    contributionsDB.findOne({ userId: parsedUserId }, (err, contributions) => {
+      if (err) return callback(err, null);
+
+      // Default contributions if not set
+      const base = contributions || { preTax: 2, afterTax: 2, roth: 2 };
+
+      userDAO.getUserById(parsedUserId, (err2, user) => {
+        if (err2) return callback(err2, null);
+
+        const result = {
+          ...base,
+          userId: parsedUserId,
+          userName: user.userName,
+          firstName: user.firstName,
+          lastName: user.lastName,
         };
 
-        contributionsDB.update({
-            userId
-            },
-            contributions, {
-                upsert: true
-            },
-            err => {
-                if (!err) {
-                    console.log("Updated contributions");
-                    // add user details
-                    userDAO.getUserById(parsedUserId, (err, user) => {
-
-                        if (err) return callback(err, null);
-
-                        contributions.userName = user.userName;
-                        contributions.firstName = user.firstName;
-                        contributions.lastName = user.lastName;
-                        contributions.userId = userId;
-
-                        return callback(null, contributions);
-                    });
-                } else {
-                    return callback(err, null);
-                }
-            }
-        );
-    };
-
-    this.getByUserId = (userId, callback) => {
-        contributionsDB.findOne({
-                userId: userId
-            },
-            (err, contributions) => {
-                if (err) return callback(err, null);
-
-                // Set defualt contributions if not set
-                contributions = contributions || {
-                    preTax: 2,
-                    afterTax: 2,
-                    roth: 2
-                };
-
-                // add user details
-                userDAO.getUserById(userId, (err, user) => {
-
-                    if (err) return callback(err, null);
-                    contributions.userName = user.userName;
-                    contributions.firstName = user.firstName;
-                    contributions.lastName = user.lastName;
-                    contributions.userId = userId;
-
-                    callback(null, contributions);
-                });
-            }
-        );
-    };
+        return callback(null, result);
+      });
+    });
+  };
 }
 
-module.exports = { ContributionsDAO };
+module.exports = { ContributionsDAO };
--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -1,80 +1,74 @@
 const ContributionsDAO = require("../data/contributions-dao").ContributionsDAO;
-const {
-    environmentalScripts
-} = require("../../config/config");
+const { environmentalScripts } = require("../../config/config");
 
 /* The ContributionsHandler must be constructed with a connected db */
 function ContributionsHandler(db) {
+  "use strict";
+
+  const contributionsDAO = new ContributionsDAO(db);
+
+  this.displayContributions = (req, res, next) => {
+    const { userId } = req.session;
+
+    contributionsDAO.getByUserId(userId, (error, contrib) => {
+      if (error) return next(error);
+
+      // set for nav menu items
+      contrib.userId = userId;
+      return res.render("contributions", {
+        ...contrib,
+        environmentalScripts,
+      });
+    });
+  };
+
+  this.handleContributionsUpdate = (req, res, next) => {
     "use strict";
 
-    const contributionsDAO = new ContributionsDAO(db);
-
-    this.displayContributions = (req, res, next) => {
-        const {
-            userId
-        } = req.session;
-
-        contributionsDAO.getByUserId(userId, (error, contrib) => {
-            if (error) return next(error);
-
-            contrib.userId = userId; //set for nav menu items
-            return res.render("contributions", {
-                ...contrib,
-                environmentalScripts
-            });
-        });
+    // ---- secure numeric parsing (no eval) ----
+    const toNumber = (v) => {
+      const n = Number(v);
+      return Number.isFinite(n) ? n : NaN;
     };
 
-    this.handleContributionsUpdate = (req, res, next) => {
+    const preTax = toNumber(req.body.preTax);
+    const afterTax = toNumber(req.body.afterTax);
+    const roth = toNumber(req.body.roth);
 
-        /*jslint evil: true */
-        // Insecure use of eval() to parse inputs
-        const preTax = eval(req.body.preTax);
-        const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
+    const { userId } = req.session;
 
-        /*
-        //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
-        const preTax = parseInt(req.body.preTax);
-        const afterTax = parseInt(req.body.afterTax);
-        const roth = parseInt(req.body.roth);
-        */
-        const {
-            userId
-        } = req.session;
+    // ---- validation: numbers, non-negative, reasonable single-field bounds ----
+    const invalidField =
+      [preTax, afterTax, roth].some((n) => Number.isNaN(n) || n < 0 || n > 30);
 
-        //validate contributions
-        const validations = [isNaN(preTax), isNaN(afterTax), isNaN(roth), preTax < 0, afterTax < 0, roth < 0]
-        const isInvalid = validations.some(validation => validation)
-        if (isInvalid) {
-            return res.render("contributions", {
-                updateError: "Invalid contribution percentages",
-                userId,
-                environmentalScripts
-            });
-        }
-        // Prevent more than 30% contributions
-        if (preTax + afterTax + roth > 30) {
-            return res.render("contributions", {
-                updateError: "Contribution percentages cannot exceed 30 %",
-                userId,
-                environmentalScripts
-            });
-        }
+    if (invalidField) {
+      return res.render("contributions", {
+        updateError: "Invalid contribution percentages",
+        userId,
+        environmentalScripts,
+      });
+    }
 
-        contributionsDAO.update(userId, preTax, afterTax, roth, (err, contributions) => {
+    // ---- business rule: total percentage <= 30 ----
+    if (preTax + afterTax + roth > 30) {
+      return res.render("contributions", {
+        updateError: "Contribution percentages cannot exceed 30 %",
+        userId,
+        environmentalScripts,
+      });
+    }
 
-            if (err) return next(err);
+    // ---- persist to DB ----
+    contributionsDAO.update(userId, preTax, afterTax, roth, (err, contributions) => {
+      if (err) return next(err);
 
-            contributions.updateSuccess = true;
-            return res.render("contributions", {
-                ...contributions,
-                environmentalScripts
-            });
-        });
-
-    };
-
+      contributions.updateSuccess = true;
+      return res.render("contributions", {
+        ...contributions,
+        environmentalScripts,
+      });
+    });
+  };
 }
 
 module.exports = ContributionsHandler;
